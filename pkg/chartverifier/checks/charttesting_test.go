5882eaccef600a965c74a502b7d33ce7165425f8
Igor Sutton <isuttonl@redhat.com>
2021-05-03 11:40:23 +0200

    refactor(tests): add config into ChartTesting testCase

--- pkg/chartverifier/checks/charttesting_test.go
+++ pkg/chartverifier/checks/charttesting_test.go
@@ -9,16 +9,29 @@ import (
 
 func TestChartTesting(t *testing.T) {
 	type testCase struct {
+		config      map[string]interface{}
 		description string
 		uri         string
 	}
 
 	testCases := []testCase{
-		{description: "valid tarball", uri: "chart-0.1.0-v3.valid.tgz"},
+		{
+			config: map[string]interface{}{
+				"buildId":           "[BUILD_ID]",
+				"upgrade":           true,
+				"skipMissingValues": true,
+				"namespace":         "default",
+				"releaseLabel":      "app.kubernetes.io/instance",
+			},
+			description: "valid tarball",
+			uri:         "chart-0.1.0-v3.valid.tgz",
+		},
 	}
 
 	for _, tc := range testCases {
 		config := viper.New()
+		_ = config.MergeConfigMap(tc.config)
+
 		t.Run(tc.description, func(t *testing.T) {
 			r, err := ChartTesting(&CheckOptions{URI: tc.uri, ViperConfig: config})
 			require.NoError(t, err)
