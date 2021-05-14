39cdf2e1545876b0d61605e571e0155be2c9936d
Igor Sutton <isuttonl@redhat.com>
2021-05-14 14:40:10 +0200

    refactor: add layer to silence 3rd party tools

--- pkg/chartverifier/checks/charttesting.go
+++ pkg/chartverifier/checks/charttesting.go
@@ -7,8 +7,8 @@ import (
 	"github.com/helm/chart-testing/v3/pkg/chart"
 	"github.com/helm/chart-testing/v3/pkg/config"
 	"github.com/helm/chart-testing/v3/pkg/exec"
-	"github.com/helm/chart-testing/v3/pkg/tool"
 	"github.com/helm/chart-testing/v3/pkg/util"
+	"github.com/redhat-certification/chart-verifier/pkg/tool"
 )
 
 // buildChartTestingConfiguration computes the chart testing related
