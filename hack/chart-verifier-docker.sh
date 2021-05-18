ad0206797392c81086e9986132e7183f46ff1432
Igor Sutton <isuttonl@redhat.com>
2021-05-18 13:09:41 +0200

    refactor: document chart-verifier-docker.sh
    Document and extract variables from the environment to overwrite
    script variables.

--- hack/chart-verifier-docker.sh
+++ hack/chart-verifier-docker.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-CHART_VERIFIER_IMAGE="quay.io/redhat-certification/chart-verifier:main"
-DOCKER="/usr/bin/docker"
+# KUBECONFIG is either the value the user has configured in the
+# environment or the default location `kubectl` uses.
+KUBECONFIG="${KUBECONFIG:-${HOME}/.kube/config}"
 
-"${DOCKER}" run --rm -i			\
-       -e KUBECONFIG=/.kube/config	\
-       -v "${HOME}/.kube":/.kube	\
-       "${CHART_VERIFIER_IMAGE}"	\
-       $*
+# CHART_VERIFIER_IMAGE is the location of the image to use to verify
+# the chart.
+CHART_VERIFIER_IMAGE="${CHART_VERIFIER_IMAGE:-quay.io/redhat-certification/chart-verifier:main}"
+
+# DOCKER contains the path for the `podman` program.
+DOCKER="${DOCKER:-/usr/bin/docker}"
+
+"${DOCKER}" run --rm -i                      \
+            -e KUBECONFIG=/.kube/config      \
+            -v "${KUBECONFIG}":/.kube/config \
+            "${CHART_VERIFIER_IMAGE}"        \
+            $*
 
