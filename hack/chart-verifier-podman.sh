33a235320231e8c45e924ece3f7447de029d37ac
Igor Sutton <isuttonl@redhat.com>
2021-05-18 12:49:21 +0200

    refactor: silence and document chart-verifier-podman.sh
    Some options have been enabled to discard journald logging errors, and
    this could be evolved to discard depending on the environment being
    executed.
    
    The following is the result of a run with this change:
    
    ```
    $ ./chart-verifier-podman.sh verify --openshift-version="4.7.5" https://dperaza4dustbit.github.io/helm-chart/psql-service-0.1.7.tgz
    apiversion: v1
    kind: verify-report
    metadata:
        tool:
            verifier-version: 1.0.0
            chart-uri: https://dperaza4dustbit.github.io/helm-chart/psql-service-0.1.7.tgz
            digest: sha256:af4d3a25672c09ce191aeaffcbbf4e357571acaf6fe199909d3109d6ea208d11
            lastCertifiedTimestamp: "2021-05-18T10:47:49.127828+00:00"
            certifiedOpenShiftVersions: 4.7.5
        chart:
            name: psql-service
            home: ""
            sources: []
            version: 0.1.7
            description: A Helm chart for a RedHat Certified PSQL
            keywords: []
            maintainers: []
            icon: ""
            apiversion: v2
            condition: ""
            tags: ""
            appversion: 10.0.0
            deprecated: false
            annotations:
                charts.openshift.io/archs: x86_64
                charts.openshift.io/name: PSQL RedHat Demo Chart
                charts.openshift.io/provider: RedHat
                charts.openshift.io/supportURL: https://github.com/dperaza4dustbit/helm-chart
            kubeversion: 1.20.0
            dependencies: []
            type: application
        chart-overrides: ""
    results:
      - check: has-readme
        type: Mandatory
        outcome: PASS
        reason: Chart has a README
      - check: not-contains-crds
        type: Mandatory
        outcome: PASS
        reason: Chart does not contain CRDs
      - check: helm-lint
        type: Mandatory
        outcome: PASS
        reason: Helm lint successful
      - check: not-contain-csi-objects
        type: Mandatory
        outcome: PASS
        reason: CSI objects do not exist
      - check: images-are-certified
        type: Mandatory
        outcome: PASS
        reason: 'Image is Red Hat certified : registry.access.redhat.com/rhscl/postgresql-10-rhel7:latest'
      - check: is-helm-v3
        type: Mandatory
        outcome: PASS
        reason: API version is V2, used in Helm 3
      - check: contains-test
        type: Mandatory
        outcome: PASS
        reason: Chart test files exist
      - check: contains-values
        type: Mandatory
        outcome: PASS
        reason: Values file exist
      - check: contains-values-schema
        type: Mandatory
        outcome: PASS
        reason: Values schema file exist
      - check: has-kubeversion
        type: Mandatory
        outcome: PASS
        reason: Kubernetes version specified
      - check: chart-testing
        type: Mandatory
        outcome: PASS
        reason: Chart tests have passed
    
    ```

--- hack/chart-verifier-podman.sh
+++ hack/chart-verifier-podman.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-CHART_VERIFIER_IMAGE="quay.io/redhat-certification/chart-verifier:main"
-PODMAN="/usr/bin/podman"
+# KUBECONFIG is either the value the user has configured in the
+# environment or the default location `kubectl` uses.
+KUBECONFIG="${KUBECONFIG:-${HOME}/.kube/config}"
 
-"${PODMAN}" run --rm -i			\
-       -e KUBECONFIG=/.kube/config	\
-       -v "${HOME}/.kube":/.kube	\
-       "${CHART_VERIFIER_IMAGE}"	\
-       $*
+# CHART_VERIFIER_IMAGE is the location of the image to use to verify
+# the chart.
+CHART_VERIFIER_IMAGE="${CHART_VERIFIER_IMAGE:-quay.io/redhat-certification/chart-verifier:main}"
+
+# PODMAN contains the path for the `podman` program.
+PODMAN="${PODMAN:-/usr/bin/podman}"
+
+"${PODMAN}" run --rm -i                      \
+            --log-level="error"              \
+            --systemd="false"                \
+            --cgroup-manager="cgroupfs"      \
+            --cgroups="disabled"             \
+            --events-backend="none"          \
+            --log-driver="none"              \
+            -e KUBECONFIG=/.kube/config      \
+            -v "${KUBECONFIG}":/.kube/config \
+            "${CHART_VERIFIER_IMAGE}"        \
+            $*
