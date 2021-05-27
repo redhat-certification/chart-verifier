97036fe50ebcaeb6379d98dc6746b93d66a11eb3
Igor Sutton <isuttonl@redhat.com>
2021-05-27 23:53:23 +0200

    refactor: integrate both positive and negative tests

--- pkg/chartverifier/checks/charttesting_test.go
+++ pkg/chartverifier/checks/charttesting_test.go
@@ -7,7 +7,9 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
+	"helm.sh/helm/v3/pkg/cli"
 )
 
 // absPathFromSourceFileLocation returns the absolute path of a file or directory under the current source file's
@@ -17,12 +19,12 @@ func absPathFromSourceFileLocation(name string) (string, error) {
 	if !ok {
 		panic("couldn't get current path")
 	}
-    filename, err := filepath.Abs(filename)
-    if err != nil {
-        return "", fmt.Errorf("retrieving current source file's location: %w", err)
-    }
-    dirname := path.Dir(filename)
-    return filepath.Join(dirname, name), nil
+	filename, err := filepath.Abs(filename)
+	if err != nil {
+		return "", fmt.Errorf("retrieving current source file's location: %w", err)
+	}
+	dirname := path.Dir(filename)
+	return filepath.Join(dirname, name), nil
 }
 
 func TestChartTesting(t *testing.T) {
@@ -32,19 +34,21 @@ func TestChartTesting(t *testing.T) {
 		opts        CheckOptions
 	}
 
-    chartUri, err := absPathFromSourceFileLocation("chart-0.1.0-v3.valid.tgz")
-    if err != nil {
-        t.Error(err)
-    }
+	chartUri, err := absPathFromSourceFileLocation("psql-service-0.1.7")
+	if err != nil {
+		t.Error(err)
+	}
 
 	positiveTestCases := []testCase{
 		{
-			description: "with license=true value override",
+			description: "providing a valid k8Project value should succeed",
 			opts: CheckOptions{
 				URI: chartUri,
 				Values: map[string]interface{}{
-					"license": true,
+					"k8Project": "default",
 				},
+				ViperConfig:     viper.New(),
+				HelmEnvSettings: cli.New(),
 			},
 		},
 	}
@@ -60,10 +64,14 @@ func TestChartTesting(t *testing.T) {
 
 	negativeTestCases := []testCase{
 		{
-			description: "with chart-testing defaults",
+			description: "providing a bogus k8Project should fail",
 			opts: CheckOptions{
 				URI: chartUri,
-				Values: map[string]interface{}{},
+				Values: map[string]interface{}{
+					"k8Project": "bogus",
+				},
+				ViperConfig:     viper.New(),
+				HelmEnvSettings: cli.New(),
 			},
 		},
 	}
@@ -71,9 +79,10 @@ func TestChartTesting(t *testing.T) {
 	for _, tc := range negativeTestCases {
 		t.Run(tc.description, func(t *testing.T) {
 			r, err := ChartTesting(&tc.opts)
-			require.Error(t, err)
 			require.NotNil(t, r)
 			require.False(t, r.Ok)
+			require.NoError(t, err)
+			require.Contains(t, r.Reason, "executing helm with args")
 		})
 	}
 }
