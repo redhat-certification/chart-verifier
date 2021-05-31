1079e428d7c57790e30e48ed8f0c8c2cd3b8338a
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-05-31 12:55:36 +0530

    Use Kubernetes version to derive OpenShift version

--- pkg/tool/oc.go
+++ pkg/tool/oc.go
@@ -17,7 +17,18 @@ func NewOc(exec exec.ProcessExecutor) Oc {
 	}
 }
 
-const osVersionKey = "openshiftVersion"
+const osVersionKey = "serverVersion"
+
+// Based on https://access.redhat.com/solutions/4870701
+var kubeOpenShiftVersionMap map[string]string = map[string]string{
+	"1.20": "4.7.0",
+	"1.19": "4.6.0",
+	"1.18": "4.5.0",
+	"1.17": "4.4.0",
+	"1.16": "4.3.0",
+	"1.14": "4.2.0",
+	"1.13": "4.1.0",
+}
 
 func (o Oc) GetVersion() (string, error) {
 	rawOutput, err := o.exec.RunProcessAndCaptureOutput("oc", "version", "-o", "yaml")
@@ -29,16 +40,14 @@ func (o Oc) GetVersion() (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	version := out[osVersionKey]
-	if version == nil {
-		return "", fmt.Errorf("%q not found in 'oc version' output", osVersionKey)
-	}
-
-	v, ok := version.(string)
+	// Relying on Kubernetes version can be replaced after fixing this issue:
+	// https://bugzilla.redhat.com/show_bug.cgi?id=1850656
+	kubeServerVersion := out[osVersionKey].(map[string]string)
+	kubeVersion := kubeServerVersion["major"] + "." + kubeServerVersion["minor"]
+	osVersion, ok := kubeOpenShiftVersionMap[kubeVersion]
 	if !ok {
-		return "", fmt.Errorf("%q does not contain a string: %v", osVersionKey, version)
+		return "", fmt.Errorf("Internal error: %q not found in Kubernetes-OpenShift version map", kubeVersion)
 	}
 
-	return v, nil
+	return osVersion, nil
 }
