9f89863931ba504f6a222218b72cea886750710f
Igor Sutton <isuttonl@redhat.com>
2021-06-04 17:45:55 +0200

    refactor: skip test if in-cluster testing is disabled

--- hack/chartverifier_test.go
+++ hack/chartverifier_test.go
@@ -1,23 +1,25 @@
 package hack
 
 import (
-	"bytes"
+	"os"
 	"os/exec"
 	"testing"
 
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 )
 
 func TestChartVerifierSh(t *testing.T) {
 	t.Run("Should succeed when the chart exists and is valid for a single check", func(t *testing.T) {
-        t.Skip()
+		if os.Getenv("CHART_VERIFIER_ENABLE_CLUSTER_TESTING") == "" {
+			t.Skip("CHART_VERIFIER_ENABLE_CLUSTER_TESTING not set, skipping in cluster tests")
+		}
+		t.Skip()
 		args := []string{
 			"-V", "4.9",
 			"../pkg/chartverifier/checks/chart-0.1.0-v3.valid.tgz",
 		}
 
-        cmdOutput, err := exec.Command("./chart-verifier.sh", args...).Output()
+		cmdOutput, err := exec.Command("./chart-verifier.sh", args...).Output()
 
 		require.NoError(t, err)
 		require.NotEmpty(t, cmdOutput)
