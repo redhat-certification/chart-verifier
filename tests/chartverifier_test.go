558bdeb3e5e7024d8382721fe16a500159503437
Igor Sutton <isuttonl@redhat.com>
2021-06-05 18:01:24 +0200

    refactor: use tool.ProcessExecutor to execute test

--- tests/chartverifier_test.go
+++ tests/chartverifier_test.go
@@ -1,12 +1,33 @@
 package hack
 
 import (
+	"fmt"
 	"os"
-	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
 	"testing"
 
+	"github.com/redhat-certification/chart-verifier/pkg/tool"
 	"github.com/stretchr/testify/require"
 )
+
+// absPathFromSourceFileLocation returns the absolute path of a file or directory under the current source file's
+// location.
+//
+// TODO: refactor this into a testutil package.
+func absPathFromSourceFileLocation(name string) (string, error) {
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("couldn't get current path")
+	}
+	filename, err := filepath.Abs(filename)
+	if err != nil {
+		return "", fmt.Errorf("retrieving current source file's location: %w", err)
+	}
+	dirname := path.Dir(filename)
+	return filepath.Join(dirname, name), nil
+}
 
 func TestChartVerifierSh(t *testing.T) {
 	t.Run("Should succeed when the chart exists and is valid for a single check", func(t *testing.T) {
@@ -14,12 +35,19 @@ func TestChartVerifierSh(t *testing.T) {
 			t.Skip("CHART_VERIFIER_ENABLE_CLUSTER_TESTING not set, skipping in cluster tests")
 		}
 
-		args := []string{
+		shPath, err := absPathFromSourceFileLocation("../hack/chart-verifier.sh")
+		require.NoError(t, err)
+
+		pkgPath, err := absPathFromSourceFileLocation("../pkg/chartverifier/checks/chart-0.1.0-v3.valid.tgz")
+		require.NoError(t, err)
+
+		args := []interface{}{
 			"-V", "4.9",
-			"../pkg/chartverifier/checks/chart-0.1.0-v3.valid.tgz",
+			pkgPath,
 		}
 
-		cmdOutput, err := exec.Command("./chart-verifier.sh", args...).Output()
+		pe := tool.NewProcessExecutor(false)
+		cmdOutput, err := pe.RunProcessInDirAndCaptureOutput(".", shPath, args...)
 
 		require.NoError(t, err)
 		require.NotEmpty(t, cmdOutput)
