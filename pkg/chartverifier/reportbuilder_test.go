628fc16373a0c20b296d0413958517246de71a25
Martin Mulholland <mmulholl@redhat.com>
2021-06-28 15:07:02 -0400

    Calculate package sha values

--- pkg/chartverifier/reportbuilder_test.go
+++ pkg/chartverifier/reportbuilder_test.go
@@ -1,6 +1,11 @@
 package chartverifier
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/redhat-certification/chart-verifier/pkg/chartverifier/checks"
@@ -40,6 +45,56 @@ func TestSha(t *testing.T) {
 				require.Equal(t, shas[chart], sha)
 			})
 		}
+	}
+
+}
+
+func TestFilePackageDigest(t *testing.T) {
+
+	charts := []string{"checks/chart-0.1.0-v3.with-csi.tgz",
+		"checks/chart-0.1.0-v3.valid.tgz",
+		"checks/chart-0.1.0-v2.invalid.tgz",
+		"checks/chart-0.1.0-v3.without-readme.tgz",
+		"checks/chart-0.1.0-v3.with-crd.tgz"}
+
+	for _, chart := range charts {
+		cmd := exec.Command("sha256sum", chart)
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("error running command:")
+		}
+		commandResponse := strings.Split(out.String(), " ")
+		assert.Equal(t, commandResponse[0], GetPackageDigest(chart), fmt.Sprintf("%s digests did not match as expected", chart))
+	}
+
+}
+
+func TestUrlPackageDigest(t *testing.T) {
+
+	charts := []string{"https://github.com/redhat-certification/chart-verifier/blob/main/pkg/chartverifier/checks/chart-0.1.0-v3.valid.tgz?raw=true",
+		"https://github.com/openshift-helm-charts/charts/releases/download/hashicorp-vault-0.13.0/hashicorp-vault-0.13.0.tgz",
+		"https://github.com/openshift-helm-charts/charts/releases/download/hashicorp-vault-0.12.0/hashicorp-vault-0.12.0.tgz",
+		"https://github.com/IBM/charts/blob/master/repo/ibm-helm/ibm-object-storage-plugin-2.1.2.tgz?raw=true"}
+
+	for _, chart := range charts {
+
+		var out bytes.Buffer
+		cmd1 := exec.Command("curl", "-L", chart)
+
+		cmd2 := exec.Command("sha256sum")
+
+		cmd2.Stdin, _ = cmd1.StdoutPipe()
+		cmd2.Stdout = &out
+
+		cmd2.Start()
+		cmd1.Run()
+		cmd2.Wait()
+
+		commandResponse := strings.Split(out.String(), " ")
+		assert.Equal(t, commandResponse[0], GetPackageDigest(chart), fmt.Sprintf("%s digests did not match as expected", chart))
+
 	}
 
 }
