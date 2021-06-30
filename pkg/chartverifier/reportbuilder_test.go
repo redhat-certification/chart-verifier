f285eb39ffb5d932c357ea04565048806366eb58
Martin Mulholland <mmulholl@redhat.com>
2021-06-30 09:47:00 -0400

    remove use of curl in tests

--- pkg/chartverifier/reportbuilder_test.go
+++ pkg/chartverifier/reportbuilder_test.go
@@ -71,29 +71,16 @@ func TestFilePackageDigest(t *testing.T) {
 
 func TestUrlPackageDigest(t *testing.T) {
 
-	charts := []string{"https://github.com/redhat-certification/chart-verifier/blob/main/pkg/chartverifier/checks/chart-0.1.0-v3.valid.tgz?raw=true",
-		"https://github.com/openshift-helm-charts/charts/releases/download/hashicorp-vault-0.13.0/hashicorp-vault-0.13.0.tgz",
-		"https://github.com/openshift-helm-charts/charts/releases/download/hashicorp-vault-0.12.0/hashicorp-vault-0.12.0.tgz",
-		"https://github.com/IBM/charts/blob/master/repo/ibm-helm/ibm-object-storage-plugin-2.1.2.tgz?raw=true"}
+	charts := make(map[string]string)
 
-	for _, chart := range charts {
+	charts["https://github.com/openshift-helm-charts/charts/releases/download/hashicorp-vault-0.13.0/hashicorp-vault-0.13.0.tgz"] = "97e274069d9d3d028903610a3f9fca892b2620f0a334de6215ec5f962328586f"
+	charts["https://github.com/openshift-helm-charts/charts/releases/download/hashicorp-vault-0.12.0/hashicorp-vault-0.12.0.tgz"] = "b07be2a554ecbe6a6dd48ea763ed568de317d17cf1a19fb11ddb562983286555"
+	charts["https://github.com/IBM/charts/blob/master/repo/ibm-helm/ibm-object-storage-plugin-2.1.2.tgz?raw=true"] = "06efa1e26f8a7ba93a6e6136650b0624af2558cc44a4588198fca322f9219e32"
+	charts["https://github.com/redhat-certification/chart-verifier/blob/main/pkg/chartverifier/checks/chart-0.1.0-v3.valid.tgz?raw=true"] = "4f29f2a95bf2b9a1c62fd215b079a01bdc5a38e9b4ff874d0fa21d0afca2e76d"
 
-		var out bytes.Buffer
-		cmd1 := exec.Command("curl", "-L", chart)
-		cmd2 := exec.Command("sha256sum")
+	for chart, sha := range charts {
 
-		var err error
-		cmd2.Stdin, err = cmd1.StdoutPipe()
-		assert.NoError(t, err, "error get pipe from curl -L command")
-		cmd2.Stdout = &out
-		err = cmd2.Start()
-		assert.NoError(t, err, "error starting sha256sum command")
-		err = cmd1.Run()
-		assert.NoError(t, err, "error starting curl -L command")
-		err = cmd2.Wait()
-		assert.NoError(t, err, "error waiting for sha256sum command")
-		commandResponse := strings.Split(out.String(), " ")
-		assert.Equal(t, commandResponse[0], GetPackageDigest(chart), fmt.Sprintf("%s digests did not match as expected", chart))
+		assert.Equal(t, sha, GetPackageDigest(chart), fmt.Sprintf("%s digests did not match as expected", chart))
 
 	}
 
