775a8be6dbdd75d2c8c201dec422f91e02766780
Martin Mulholland <mmulholl@redhat.com>
2021-10-20 14:16:51 -0400

    enable chart verifier to recognize OCP 4.9

--- pkg/tool/oc_test.go
+++ pkg/tool/oc_test.go
@@ -1,41 +1,58 @@
 package tool
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type TestData struct {
+	getVersionOut string
+	OCVersion     string
+}
 
 type fakeProcessExecutor struct {
 	Output string
 }
 
-var output47 = `
+var output120 = `
 serverVersion:
   major: "1"
   minor: "20"
 `
+var output121 = `
+serverVersion:
+  major: "1"
+  minor: "21"
+`
+var output122 = `
+serverVersion:
+  major: "1"
+  minor: "22"
+`
+
+var testsData []TestData
 
 func (p fakeProcessExecutor) RunProcessAndCaptureOutput(executable string, execArgs ...interface{}) (string, error) {
 	return p.Output, nil
 }
 
-func TestOcVersion47(t *testing.T) {
-	fp := fakeProcessExecutor{Output: output47}
-	oc := NewOc(fp)
-	version, _ := oc.GetVersion()
-	if version != "4.7.0" {
-		t.Error("Version mismatch", version)
+func TestOCVersions(t *testing.T) {
+
+	testsData = append(testsData, TestData{getVersionOut: output120, OCVersion: "4.7.0"})
+	testsData = append(testsData, TestData{getVersionOut: output121, OCVersion: "4.8.0"})
+	testsData = append(testsData, TestData{getVersionOut: output122, OCVersion: "4.9.0"})
+
+	for _, testdata := range testsData {
+		t.Logf("Check for OC %s", testdata.OCVersion)
+		checkOcVersion(testdata.getVersionOut, testdata.OCVersion, t)
 	}
 }
 
-var output48 = `
-serverVersion:
-  major: "1"
-  minor: "21"
-`
-
-func TestOcVersion48(t *testing.T) {
-	fp := fakeProcessExecutor{Output: output48}
+func checkOcVersion(kubeVersion string, expectedOCversion string, t *testing.T) {
+	fp := fakeProcessExecutor{Output: kubeVersion}
 	oc := NewOc(fp)
 	version, _ := oc.GetVersion()
-	if version != "4.8.0" {
-		t.Error("Version mismatch", version)
+	if version != expectedOCversion {
+		t.Error(fmt.Sprintf("Version mismatch expected: %s, got: %s", expectedOCversion, version))
 	}
 }
