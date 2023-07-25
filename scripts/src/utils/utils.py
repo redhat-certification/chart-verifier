2ac8ee4377ec95f070bed087f0161bc7f887ae9a
mgoerens <mgoerens@redhat.com>
2023-07-25 18:42:55 +0200

    Print step's output paramaters also to sdtout
    This commit makes sure that all steps' output parameters (i.e.
    GITHUB_OUTPUT) are also printed to stdout, making it easier to debug
    workflow runs.
    
    It also modifies the scripts output logic to only write to GITHUB_OUTPUT
    if the environment variable is defined, making it easier to run the
    scripts locally.
    
    close #388
    
    Signed-off-by: mgoerens <mgoerens@redhat.com>

--- scripts/src/utils/utils.py
+++ scripts/src/utils/utils.py
@@ -1,5 +1,13 @@
 import os
 
-def add_output(name,value):
-    with open(os.environ['GITHUB_OUTPUT'],'a') as fh:
-        print(f'{name}={value}',file=fh)
+def add_output(key,value):
+    """This function prints the key/value pair to stdout in a "key=value" format.
+
+    If called from a GitHub workflow, it also sets an output parameter.
+    """
+
+    print(f'{key}={value}')
+
+    if "GITHUB_OUTPUT" in os.environ:
+        with open(os.environ['GITHUB_OUTPUT'],'a') as fh:
+            print(f'{key}={value}',file=fh)
